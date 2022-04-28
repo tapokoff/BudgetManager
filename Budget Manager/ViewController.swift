51ae7ac3ae334e27a667644e8d179bf3c976eb4b
Daniil <Balakiriev>
2022-04-29 01:45:15 +0200

    Initial Commit

---
+++ Budget Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Budget Manager
+//
+//  Created by Irishka Balakireva on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
